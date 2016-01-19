28ef76e34a374fa2cc7223110f097611b21caf6e
ilker ALTIN <ilkerobi@gmail.com>
2016-01-19 17:20:50 +0200

    get videos from youtube api

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 class SearchBar extends Component{
   constructor(props){
     super(props);
-    this.state={term:'merhaba dünya'};
+    this.state={term:''};
      this.onInputChange = this.onInputChange.bind(this)
   }
   onInputChange(event){
@@ -17,7 +17,6 @@ this.setState({
       <input
         value={this.state.term}
         onChange={this.onInputChange} />
-      <h2>value of state:{this.state.term}</h2>
     </div>
     );
   };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 const API_KEY='AIzaSyDrNuclx6sQ4cPw-zhWWU1_WA_KQYEsci8';
-const App=()=> {
-  return (
-    <div><h1>Selam:</h1>
-    <SearchBar />
-    </div>
-);
-}
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state={videos:[],ilker:'deneme'};
+
+    YTSearch({key:API_KEY,term:'surfboards'}, (data)=>{
+        this.setState({videos:data});
+        //this.setState=({videos:video});
+    });
+  }
+  render(){
+            return (
+              <div>
+                <h1>{this.state.videos.length}</h1>
+              <SearchBar />
+              <VideoList videos={this.state.videos}/>
+              </div>
+              );
+          }
+    }
 
 ReactDOM.render(<App />,document.querySelector('.container'));
