168b08c9cb41ddf843cf5101b876c926c2afa666
ilker ALTIN <ilkerobi@gmail.com>
2016-01-19 12:19:01 +0200

    state added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,24 @@
 import React,{Component} from 'react';
 
-class SearchBar extends React.Component{
+class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state={term:'merhaba dünya'};
+     this.onInputChange = this.onInputChange.bind(this)
+  }
+  onInputChange(event){
+this.setState({
+    term:event.target.value
+  })
+  }
   render(){
     return(
-      <input />
+      <div>
+      <input
+        value={this.state.term}
+        onChange={this.onInputChange} />
+      <h2>value of state:{this.state.term}</h2>
+    </div>
     );
   };
 }
