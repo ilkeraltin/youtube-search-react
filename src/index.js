d0a973832e3eac64a621dc103773bfc3f0febb3c
ilker ALTIN <ilkerobi@gmail.com>
2016-01-19 17:32:38 +0200

    video detail page coded

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 const API_KEY='AIzaSyDrNuclx6sQ4cPw-zhWWU1_WA_KQYEsci8';
 
@@ -11,16 +12,18 @@ class App extends Component{
 
     this.state={videos:[],ilker:'deneme'};
 
-    YTSearch({key:API_KEY,term:'surfboards'}, (data)=>{
-        this.setState({videos:data});
+    YTSearch({key:API_KEY,term:'surfboards'}, (videos)=>{
+        this.setState({videos});
         //this.setState=({videos:video});
     });
   }
+
   render(){
             return (
               <div>
                 <h1>{this.state.videos.length}</h1>
               <SearchBar />
+              <VideoDetail video={this.state.videos[0]}/>
               <VideoList videos={this.state.videos}/>
               </div>
               );
